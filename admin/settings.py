4e3ba4b87a354baa35ce977498c7b5bcadfa0b39
nawarkhede <nishant.nawarkhede@gmail.com>
2021-02-24 01:42:34 +0530

    local settings import added

--- admin/settings.py
+++ admin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "products",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+try:
+    from local_settings import *  # noqa
+except ImportError:
+    pass
