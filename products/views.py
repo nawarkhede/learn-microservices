2eeeb6adcfa9e7f0cb1979224916f72e9b9728ec
nawarkhede <nishant.nawarkhede@gmail.com>
2021-02-24 09:12:19 +0530

    imports optimized

--- products/views.py
+++ products/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
-
 # Create your views here.
 
 from rest_framework import viewsets, status
+from rest_framework.response import Response
+
 from .models import Product
 from .serializers import ProductSerializer
-from rest_framework.response import Response
 
 
 class ProductViewSet(viewsets.ViewSet):
