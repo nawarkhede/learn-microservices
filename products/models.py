0b23e71388aedd2cd6119063c3a220a849a75938
nawarkhede <nishant.nawarkhede@gmail.com>
2021-02-24 21:39:16 +0530

    test commit

--- products/models.py
+++ products/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class Product(models.Model):
     title = models.CharField(max_length=200)
     image = models.CharField(max_length=200)
