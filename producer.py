a47e67ae8d5abe15a9d94a17f9eb5ad0b7ffb829
nawarkhede <nishant.nawarkhede@gmail.com>
2021-02-25 13:23:02 +0530

    working pubsub

--- producer.py
+++ producer.py
@@ -1,4 +1,5 @@
 import pika
+import json
 
 params = pika.URLParameters(
     "amqps://mhxyppjb:VwK96Emr2dpRfbmiPFcLNoVKecyQQy8o@lionfish.rmq.cloudamqp.com/mhxyppjb"
@@ -8,6 +9,16 @@ connection = pika.BlockingConnection(parameters=params)
 
 channel = connection.channel()
 
+channel.queue_declare(queue="anotheradmin")
 
-def publish():
-    channel.basic_publish(exchange="", routing_key="flaskadmin", body="hello django")
+
+def publish(method, body):
+    for i in range(10):
+        print("Im in publish", body)
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(
+        exchange="",
+        routing_key="anotheradmin",
+        body=json.dumps(body),
+        properties=properties,
+    )
